976e6cad96379d3fe0b207c5229354bb4ea3ad2d
FERNANDO <FERNANDO@192.168.0.4>
2021-09-13 15:56:34 -0500

    ahi si

--- src/main/java1/deber/nonacces/EmpleadoRecursosHumanos.java
+++ src/main/java1/deber/nonacces/EmpleadoRecursosHumanos.java
@@ -3,7 +3,7 @@ package deber.nonacces;
 public class EmpleadoRecursosHumanos extends Empleado {
 
 	protected final void realizarPagos() {
-		System.out.println("El estudiante de medicina realiza 50 horas de practica");
+		System.out.println("Realizar el Pago el dia que corresponde");
 	}
 	
 	
--- src/main/java1/deber/nonacces/EmpleadoRecursosHumanosPasante.java
+++ src/main/java1/deber/nonacces/EmpleadoRecursosHumanosPasante.java
@@ -4,6 +4,6 @@ public class EmpleadoRecursosHumanosPasante extends EmpleadoRecursosHumanos{
 
 	//no se puede socreescribir metodos en una clase hija 
 	public void realizarPagos() {
-		System.out.println("El estudiante de medicina realiza 50 horas de practica");
+		System.out.println("Realizar el Pago el dia que corresponde");
 	}
 }
